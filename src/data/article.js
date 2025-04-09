37bbca571f993ced7a9c8dbec7d2f2c7a24d1b36
Carlo Costa <carlo.costa92@gmail.com>
2025-04-09 15:26:37 +0200

    feat: create new folder for data

---
+++ src/data/article.js
@@ -0,0 +1,14 @@
+const Article = [
+    "Ricette Vegane Facili e Veloci",
+    "Guida Completa al Giardinaggio Urbano",
+    "I 10 Libri Fantasy Imperdibili del 2023",
+    "Consigli di Viaggio per un Weekend a Roma",
+    "Come Avviare un'Attività Online di Successo",
+    "Le Migliori Tecniche di Meditazione per Principianti",
+    "Storia e Curiosità dei Cocktail Classici",
+    "Fotografia di Paesaggio: Consigli e Trucchi",
+    "Il Mondo dei Videogiochi Indie: Recensioni e Novità",
+    "Fitness a Casa: Esercizi Efficaci Senza Attrezzi"
+];
+
+export default Article;
